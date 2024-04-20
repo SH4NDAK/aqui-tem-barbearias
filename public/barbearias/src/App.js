40a289a96b8959095dce8f7be5f34de2daad7f4c
Arthur Marena <arthurzhm2@gmail.com>
2024-04-20 00:19:48 -0300

    Instalando o lucide-react

--- public/barbearias/src/App.js
+++ public/barbearias/src/App.js
@@ -1,14 +1,15 @@
 
 import { useState } from "react";
 import InputText from './components/InputText';
+import { Camera, Eye } from 'lucide-react'
 
 export default function App() {
-  
+
   // usestate é bascimente o estado de um elemento
-  
+
   // estado login (valor dele)
   const [login, setLogin] = useState(null);
-  console.log(login);
+  const [senha, setSenha] = useState(null);
 
   return (
     <div className="flex justify-center items-center w-screen h-dvh bg-[#242222]">
@@ -16,14 +17,24 @@ export default function App() {
         <div className="w-full flex justify-center font-bold text-5xl">
           LOGO
         </div>
-        <div className="w-full flex flex-col">
+        <div className="w-full flex flex-col gap-2">
           <InputText
             label={'Login'}
             placeholder={'E-mail ou telefone'}
             type={'text'}
             value={login}
-            onChange={(e) => {setLogin(e.currentTarget.value)}}
+            onChange={(e) => { setLogin(e.currentTarget.value) }}
           />
+
+          <InputText
+            label={'Senha'}
+            type={'password'}
+            className=""
+            placeholder={'Digite sua senha'}
+            value={senha}
+            onChange={(e) => { setSenha(e.currentTarget.value) }}
+          />
+          <Eye/>
         </div>
       </div>
 
--- public/barbearias/src/components/InputText.js
+++ public/barbearias/src/components/InputText.js
@@ -3,20 +3,19 @@ import React from 'react';
 
 const InputText = ( {label, type, placeholder, ...props} ) => {
     return (
-        <>
+        <div>
             <label
                 className="font-semibold text-sm"
             >
                 {label}
             </label>
             <input
-
                 className="w-full border border-[#242222] rounded-sm p-1 font-[#242222] outline-none"
                 placeholder={placeholder}
                 type={type}
                 {...props}
             />
-        </>
+        </div>
     )
 }
 
