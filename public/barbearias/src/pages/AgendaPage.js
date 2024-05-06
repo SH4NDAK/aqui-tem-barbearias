6aa0d7997e2cb0cf24b1d3fcd1f8e3d4cac32b48
Arthur Marena <112282554+SH4NDAK@users.noreply.github.com>
2024-05-06 14:46:42 -0300

    Configuraçoes iniciais agenda

--- public/barbearias/src/pages/AgendaPage.js
+++ public/barbearias/src/pages/AgendaPage.js
@@ -1,8 +1,51 @@
+import React, { useCallback, useState } from 'react';
+import Scheduler, { Resource } from 'devextreme-react/scheduler';
+import { Switch } from 'devextreme-react/switch';
+import { NumberBox } from 'devextreme-react/number-box';
 
-import Container from "../components/Container"
+const currentDate = new Date();
+const views = ['day', 'week', 'month'];
 
-export default function AgendaPage() {
+const onContentReady = (e) => {
+    e.component.scrollTo(new Date());
+};
+const onAppointmentClick = (e) => {
+    e.cancel = true;
+};
+const onAppointmentDblClick = (e) => {
+    e.cancel = true;
+};
+const App = () => {
+    const [showCurrentTimeIndicator, setShowCurrentTimeIndicator] = useState(true);
+    const [shadeUntilCurrentTime, setShadeUntilCurrentTime] = useState(true);
+    const [updateInterval, setUpdateInterval] = useState(10);
+    const onShowCurrentTimeIndicatorChanged = useCallback((e) => {
+        setShowCurrentTimeIndicator(e.value);
+    }, []);
+    const onShadeUntilCurrentTimeChanged = useCallback((e) => {
+        setShadeUntilCurrentTime(e.value);
+    }, []);
+    const onUpdateIntervalChanged = useCallback((e) => {
+        setUpdateInterval(e.value);
+    }, []);
     return (
-        <></>
-    )
-}
+        <React.Fragment>
+            <Scheduler
+                locale="pt-BR"
+                views={views}
+                defaultCurrentView="week"
+                showCurrentTimeIndicator={showCurrentTimeIndicator}
+                indicatorUpdateInterval={updateInterval * 1000}
+                showAllDayPanel={false}
+                shadeUntilCurrentTime={shadeUntilCurrentTime}
+                defaultCurrentDate={currentDate}
+                editing={false}
+                height={600}
+                onContentReady={onContentReady}
+                onAppointmentClick={onAppointmentClick}
+                onAppointmentDblClick={onAppointmentDblClick}
+            >
+            </Scheduler>    </React.Fragment>
+    );
+};
+export default App;
