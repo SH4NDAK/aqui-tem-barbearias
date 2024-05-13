74d880d05a9d459f3fa01a9efcfd58cccb72ac88
Leonardo <leonardovbalves@hotmail.com>
2024-05-13 14:26:28 -0300

    Agendamento finalizado

--- public/barbearias/src/pages/AgendaPage.js
+++ public/barbearias/src/pages/AgendaPage.js
@@ -25,7 +25,7 @@ export default function AgendaPage() {
     const Appointment = (e) => {
         let findAgenda = agendas.find(i => i.id === e.data.targetedAppointmentData.assigneeId) || {};
         const [agenda, setAgenda] = useState(findAgenda);
-    
+
         const onChangePay = async () => {
             try {
                 // Alterar o valor de pago
@@ -35,13 +35,13 @@ export default function AgendaPage() {
                     ativo: agenda.ativo.toString(),
                     preco: agenda.preco.toString()
                 };
-    
+
                 // Atualizar o estado com o novo valor de pago
                 setAgenda(newAgenda);
-    
+
                 // Enviar a solicitação para editar a agenda com o novo valor de pago
-                const res = await editAgenda(findAgenda.id, {...newAgenda, pago: newAgenda.pago.toString()});
-    
+                const res = await editAgenda(findAgenda.id, { ...newAgenda, pago: newAgenda.pago.toString() });
+
                 if (newAgenda.pago) {
                     notification.success({
                         message: "Sucesso ao pagar",
@@ -52,7 +52,7 @@ export default function AgendaPage() {
                 console.log(error);
             }
         };
-    
+
         return (
             <div className='flex flex-row'>
                 <div>
@@ -63,108 +63,93 @@ export default function AgendaPage() {
                     </div>
                 </div>
                 <div className="dx-scheduler-agenda-appointment-right-layout">
-                    Pago? <Switch defaultChecked={agenda.pago} onChange={onChangePay} className='h-6'/>
+                    Pago? <Switch defaultChecked={agenda.pago} onChange={onChangePay} className='h-6' />
                 </div>
             </div>
         );
     };
-    
+
 
     const onAppointmentFormOpening = (e) => { // quando a edição de agenda estiver pronto retornar isso
         let findAgenda = agendas.find(i => i.id === e.appointmentData.assigneeId) || {};
         e.form.option('items', [
-        {
-          label: {
-            text: 'Cliente',
-          },
-          editorType: 'dxSelectBox',
-          dataField: 'nomeDoCliente',
-          editorOptions: {
-            valueExpr: 'nomeDoCliente',
-            value: findAgenda.nomeDoCliente,
-          },
-        },
-        {
-          label: {
-            text: 'Serviços',
-          },
-          editorType: 'dxSelectBox',
-          dataField: 'nomeDoCliente',
-          editorOptions: {
-            valueExpr: 'nomeDoCliente',
-            value: findAgenda.nomeDoCliente,
-          },
-        },
-        {
-          label: {
-            text: 'Valor',
-          },
-          editorType: 'dxSelectBox',
-          dataField: 'nomeDoCliente',
-          editorOptions: {
-            valueExpr: 'nomeDoCliente',
-            value: findAgenda.nomeDoCliente,
-          },
-        },
-        {
-          label: {
-            text: 'Duração',
-          },
-          editorType: 'dxSelectBox',
-          dataField: 'nomeDoCliente',
-          editorOptions: {
-            valueExpr: 'nomeDoCliente',
-            value: findAgenda.nomeDoCliente,
-          },
-        },
-        {
-          label: {
-            text: 'Dia',
-          },
-          editorType: 'dxSelectBox',
-          dataField: 'nomeDoCliente',
-          editorOptions: {
-            valueExpr: 'nomeDoCliente',
-            value: findAgenda.nomeDoCliente,
-          },
-        },
-        {
-          label: {
-            text: 'Horario',
-          },
-          editorType: 'dxSelectBox',
-          dataField: 'nomeDoCliente',
-          editorOptions: {
-            valueExpr: 'nomeDoCliente',
-            value: findAgenda.nomeDoCliente,
-          },
-        },
-        {
-          label: {
-            text: 'Observação (opcional)',
-          },
-          editorType: 'dxSelectBox',
-          dataField: 'nomeDoCliente',
-          editorOptions: {
-            width: '100%',
-            type: 'datetime',
-            valueExpr: 'nomeDoCliente',
-            value: findAgenda.nomeDoCliente,
-          },
-        },
-        {
-          label: {
-            text: 'Valor Pago',
-          },
-          editorType: 'dxSelectBox',
-          dataField: 'nomeDoCliente',
-          editorOptions: {
-            width: '100%',
-            type: 'datetime',
-            valueExpr: 'nomeDoCliente',
-            value: findAgenda.nomeDoCliente,
-          },
-        },
+            {
+                label: {
+                    text: 'Cliente',
+                },
+                editorType: 'dxTextBox',
+                dataField: 'nomeDoCliente',
+                editorOptions: {
+                    valueExpr: 'nomeDoCliente',
+                    value: findAgenda.nomeDoCliente,
+                },
+            },
+            {
+                label: {
+                    text: 'Serviços',
+                },
+                editorType: 'dxTextBox',
+                dataField: 'servico',
+                editorOptions: {
+                    valueExpr: 'servico',
+                    value: findAgenda.servico,
+                },
+            },
+            {
+                label: {
+                    text: 'Valor',
+                },
+                editorType: 'dxTextBox',
+                dataField: 'preco',
+                editorOptions: {
+                    valueExpr: 'preco',
+                    value: findAgenda.preco,
+                },
+            },
+            {
+                name: 'Duração',
+                dataField: 'duracao',
+                editorType: 'dxDateBox',
+                editorOptions: {
+                    width: '100%',
+                    type: 'time',
+                    valueExpr: 'duracao',
+                    value: findAgenda.duracao
+                },
+            },
+            {
+                name: 'Dia',
+                dataField: 'dia',
+                editorType: 'dxDateBox',
+                editorOptions: {
+                    width: '100%',
+                    type: 'date',
+                    valueExpr: 'data',
+                    value: findAgenda.data
+                },
+            },
+            {
+                name: 'Horario',
+                dataField: 'horario',
+                editorType: 'dxDateBox',
+                editorOptions: {
+                    width: '100%',
+                    type: 'time',
+                    valueExpr: 'horario',
+                    value: findAgenda.horario
+                },
+            },
+            {
+                name: 'Descrição (Opcional)',
+                dataField: 'descricao',
+                editorType: 'dxTextBox',
+                editorOptions: {
+                    width: '100%',
+                    type: 'text',
+                    valueExpr: 'descricao',
+                    value: findAgenda.descricao
+                },
+            }
         ]);
     };
 
@@ -173,7 +158,6 @@ export default function AgendaPage() {
     }
 
     const onAppointmentFormDelete = async (e) => {
-        console.log(e);
         let findAgenda = agendas.find(i => i.id === e.appointmentData.assigneeId) || {};
         try {
             // Alterar o valor de pago
@@ -198,8 +182,41 @@ export default function AgendaPage() {
             console.log(error);
         }
     }
-    
-//appointmentData.assigneeId
+
+    const onAppointmentUpdated = async (e) => {
+        console.log(e.component._editAppointmentData);
+        let findAgenda = agendas.find(i => i.id === e.appointmentData.assigneeId) || {};
+        try {
+            const { nomeDoCliente, servico, descricao, ativo, pago, preco, duracao, horario, dia } = e.component._editAppointmentData
+            // Alterar o valor de pago
+            const newAgenda = {
+                ...findAgenda,
+                nomeDoCliente: nomeDoCliente,
+                servico: servico,
+                descricao: descricao,
+                ativo: findAgenda.ativo.toString(),
+                pago: findAgenda.pago.toString(),
+                preco: preco.toString(),
+                duracao: duracao,
+                horario: horario,
+                data: dia
+            };
+
+
+            // // Enviar a solicitação para editar a agenda com o novo valor de pago
+            const res = await editAgenda(findAgenda.id, newAgenda);
+
+            if (newAgenda.pago) {
+                notification.success({
+                    message: "Sucesso ao Editar",
+                    description: res.mensagem
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    //appointmentData.assigneeId
     // setando o local como brasil
     useEffect(() => {
         try {
@@ -215,27 +232,27 @@ export default function AgendaPage() {
 
     // a altura da agenda, sera o tamanho da tela do usuario -150 pixels
     const height = window.innerHeight - 150;
-      
+
     // função para calcular a data final com base na data e hora de início e na duração
     function calcularEndDate(data, horario, duracao) {
         const [horasInicio, minutosInicio] = horario.split(':').map(Number);
         const [ano, mes, dia] = data.split('-').map(Number); // Ajustando para o formato 'AAAA-MM-DD'
-    
+
         const dataInicio = new Date(ano, mes - 1, dia, horasInicio, minutosInicio);
-        
+
         // Separando a duração em horas e minutos
         const [horasDuracao, minutosDuracao] = duracao.split(':').map(Number);
-    
+
         // Calculando o tempo total em minutos
         const totalMinutos = horasDuracao * 60 + minutosDuracao;
-    
+
         // Adicionando os minutos de duração ao horário de início
         const endDate = new Date(dataInicio.getTime() + totalMinutos * 60 * 1000);
-    
+
         return endDate;
     }
-    
-    
+
+
 
     return (
         <LayoutPage>
@@ -253,16 +270,16 @@ export default function AgendaPage() {
                     <div className='w-full h-full bg-white rounded-b-md'>
                         <Scheduler
                             timeZone="America/Sao_Paulo"
-                            
+
                             dataSource={agendas?.map(compromisso => {
                                 return {
-                                  text: compromisso.servico,
-                                  startDate: new Date(compromisso.data + 'T' + compromisso.horario),
-                                  endDate: calcularEndDate(compromisso.data, compromisso.horario, compromisso.duracao),
-                                  assigneeId: compromisso.id, // ou qualquer outra propriedade que desejar usar
-                                  priorityId: 1, // ou qualquer outra propriedade que desejar usar
+                                    text: compromisso.servico,
+                                    startDate: new Date(compromisso.data + 'T' + compromisso.horario),
+                                    endDate: calcularEndDate(compromisso.data, compromisso.horario, compromisso.duracao),
+                                    assigneeId: compromisso.id, // ou qualquer outra propriedade que desejar usar
+                                    priorityId: 1, // ou qualquer outra propriedade que desejar usar
                                 };
-                              })}
+                            })}
                             views={views}
                             currentView="agenda"
                             defaultCurrentDate={currentDate}
@@ -270,6 +287,7 @@ export default function AgendaPage() {
                             appointmentComponent={Appointment}
                             onAppointmentDeleted={onAppointmentFormDelete}
                             onAppointmentFormOpening={onAppointmentFormOpening}
+                            onAppointmentUpdated={onAppointmentUpdated}
                         />
                     </div>
                 </div>
@@ -286,7 +304,7 @@ export default function AgendaPage() {
 
 const ModalAgendamento = ({ onClose }) => {
     // trazendo as operações de formulário da biblioteca hook form
-    const { register, handleSubmit,  formState: { errors }, setValue } = useForm();
+    const { register, handleSubmit, formState: { errors }, setValue } = useForm();
 
     const onSubmit = async (data) => {
         try {
