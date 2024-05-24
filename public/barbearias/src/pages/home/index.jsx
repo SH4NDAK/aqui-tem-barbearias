9a9854390491aa5a33ea18189006ad449c813b94
Arthur Marena <112282554+SH4NDAK@users.noreply.github.com>
2024-05-24 15:28:55 -0300

    Começando menu principal

--- public/barbearias/src/pages/home/index.jsx
+++ public/barbearias/src/pages/home/index.jsx
@@ -1,33 +1,79 @@
 import { useState } from "react";
 import { Button, Space } from 'antd';
 import { useNavigate } from "react-router-dom";
-import { ArrowRightCircle, Calendar } from "lucide-react";
+import { ArrowRightCircle, Calendar, Scissors, User } from "lucide-react";
 
 
 export default function Home() {
     const navigate = useNavigate();
 
+    
     const handleAgendaClick = () => {
-
+        navigate("/agenda")
     }
 
+    const handleTipoServicoClick = () => {
+        navigate("/tipos-servico")
+    }
+
+    const handleUsuariosClick = () => {
+        navigate("/usuarios")
+    }
 
     return (
         <div className="w-full h-dvh bg-[#242222]">
             <div className="flex justify-center items-center p-1">
-                <div className="p-2 flex flex-col w-1/2 bg-white h-1/2 shadow-sm shadow-black rounded-sm">
+                <div className="p-2 flex flex-col md:w-1/2 sm:w-full bg-white h-1/2 shadow-sm shadow-black rounded-md">
                     <div className="flex justify-center mb-4">
-                        <span className="text-5xl font-semibold">Menu principal</span>
+                        <span className="md:text-5xl sm:text-4xl font-semibold">Menu principal</span>
                     </div>
-                    <div className="w-full flex md:flex-row">
-                        <div className="p-2 w-48 bg-gray-200 rounded-sm shadow-sm shadow-[#242222]">
+                    <div className="flex justify-center w-full">
+                        <div className="bg-[#242222] h-0.5 w-11/12 mt-1 mb-4 opacity-5"></div>
+                    </div>
+                    <div className="w-full flex sm:flex-col gap-4 md:flex-row justify-center flex-grow-0">
+                        <div
+                            className="p-2 bg-gray-200 md:w-48 sm:w-full rounded-sm shadow-sm shadow-[#242222] cursor-pointer hover:scale-105 transition-all"
+                            onClick={handleAgendaClick}
+                        >
                             <div className="flex flex-row items-center">
                                 <Calendar className="me-1" /> <span className="font-semibold">Agenda</span>
                             </div>
                             <div className="w-full mt-3">
-                                <button 
-                                    className="flex flex-row justify-center w-full bg-[#242222] rounded-md shadow-sm shadow-[#242222] p-1.5 text-white font-semibold text-end"
+                                <button
+                                    className="flex flex-row justify-center w-full bg-[#242222] rounded-md shadow-sm shadow-[#242222] p-1.5 text-white font-semibold text-end hover:bg-[#1b1919] transition-colors"
                                     onClick={handleAgendaClick}
+                                >
+                                    Acessar <ArrowRightCircle className="ms-1" />
+                                </button>
+                            </div>
+                        </div>
+                        <div
+                            className="p-2 bg-gray-200 md:w-48 sm:w-full rounded-sm shadow-sm shadow-[#242222] cursor-pointer hover:scale-105 transition-all"
+                            onClick={handleTipoServicoClick}
+                        >
+                            <div className="flex flex-row items-center">
+                                <Scissors className="me-1" /> <span className="font-semibold">Tipos de serviço</span>
+                            </div>
+                            <div className="w-full mt-3">
+                                <button
+                                    className="flex flex-row justify-center w-full bg-[#242222] rounded-md shadow-sm shadow-[#242222] p-1.5 text-white font-semibold text-end hover:bg-[#1b1919] transition-colors"
+                                    onClick={handleTipoServicoClick}
+                                >
+                                    Acessar <ArrowRightCircle className="ms-1" />
+                                </button>
+                            </div>
+                        </div>
+                        <div
+                            className="p-2 bg-gray-200 md:w-48 sm:w-full rounded-sm shadow-sm shadow-[#242222] cursor-pointer hover:scale-105 transition-all"
+                            onClick={handleUsuariosClick}
+                        >
+                            <div className="flex flex-row items-center">
+                                <User className="me-1" /> <span className="font-semibold">Barbeiros</span>
+                            </div>
+                            <div className="w-full mt-3">
+                                <button
+                                    className="flex flex-row justify-center w-full bg-[#242222] rounded-md shadow-sm shadow-[#242222] p-1.5 text-white font-semibold text-end hover:bg-[#1b1919] transition-colors"
+                                    onClick={handleUsuariosClick}
                                 >
                                     Acessar <ArrowRightCircle className="ms-1" />
                                 </button>
