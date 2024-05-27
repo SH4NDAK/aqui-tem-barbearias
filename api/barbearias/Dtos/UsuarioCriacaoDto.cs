928b10269db0a117cf5681d2533b8b591bd222e9
Guilherme Bisaio <guilhermebisaio.curric@gmail.com>
2024-05-27 14:22:39 -0300

    Autenticação número de telefone Login/Cadastro

--- api/barbearias/Dtos/UsuarioCriacaoDto.cs
+++ api/barbearias/Dtos/UsuarioCriacaoDto.cs
@@ -1,19 +1,58 @@
 ﻿using jwtRegisterLogin.Enum;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace jwtRegisterLogin.Dtos
 {
-    //Modelo de Criacao do Usuario
+    // Atributo de validação para verificar se o valor é um email ou número de telefone válido
+    public class ValidateEmailOrPhoneNumberAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string emailOrPhoneNumber = value as string;
+
+            // Verifica se o valor é um número de telefone ou um endereço de e-mail
+            if (IsValidEmail(emailOrPhoneNumber) || IsValidPhoneNumber(emailOrPhoneNumber))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("O campo deve ser um endereço de email ou número de telefone válido.");
+            }
+        }
+        
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private bool IsValidPhoneNumber(string phoneNumber)
+        {
+            return phoneNumber.Length >= 6 && phoneNumber.Length <= 15 && phoneNumber.All(char.IsDigit);
+        }
+    }
+
     public class UsuarioCriacaoDto
     {
         [Required(ErrorMessage = "O campo usuário é obrigatório")]
         public string Usuario { get; set; }
 
-        [Required(ErrorMessage = "O campo email é obrigatório"), EmailAddress(ErrorMessage = "Email inválido!")]
+        [Required(ErrorMessage = "O campo email ou número de telefone é obrigatório")]
+        [ValidateEmailOrPhoneNumber(ErrorMessage = "Email ou número de telefone inválido!")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "O campo senha é obrigatório")]
         public string Senha { get; set; }
+
         [Required(ErrorMessage = "O campo cargo é obrigatório")]
         public CargoEnum Cargo { get; set; }
     }
--- api/barbearias/obj/Debug/net8.0/jwtRegisterLogin.AssemblyInfo.cs
+++ api/barbearias/obj/Debug/net8.0/jwtRegisterLogin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jwtRegisterLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b01aaf66ed44c64ee7efa91929e7d8f0620bfd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58b5a16888b76412bce74ddf1c0ad85f6260e594")]
 [assembly: System.Reflection.AssemblyProductAttribute("jwtRegisterLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jwtRegisterLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
