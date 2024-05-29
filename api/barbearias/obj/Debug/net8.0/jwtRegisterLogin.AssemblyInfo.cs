0a22d10125901eaa657a9edc902504899523346b
Arthur Marena <112282554+SH4NDAK@users.noreply.github.com>
2024-05-29 13:36:28 -0300

    nao sei que isso

--- api/barbearias/obj/Debug/net8.0/jwtRegisterLogin.AssemblyInfo.cs
+++ api/barbearias/obj/Debug/net8.0/jwtRegisterLogin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jwtRegisterLogin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b01aaf66ed44c64ee7efa91929e7d8f0620bfd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55591ac6fd00d9025f834c5a588470686ff84389")]
 [assembly: System.Reflection.AssemblyProductAttribute("jwtRegisterLogin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jwtRegisterLogin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
