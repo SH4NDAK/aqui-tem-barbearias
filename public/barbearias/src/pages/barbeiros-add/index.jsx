65733a3a1f59c3395207d676faba22da7ef2e664
Arthur Marena <112282554+SH4NDAK@users.noreply.github.com>
2024-06-03 14:38:33 -0300

    Começando formulário de cadastro

--- public/barbearias/src/pages/barbeiros-add/index.jsx
+++ public/barbearias/src/pages/barbeiros-add/index.jsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header";
+import InputText from "../../components/InputText";
 
 export default function BarbeirosForm() {
     return (
@@ -6,6 +7,20 @@ export default function BarbeirosForm() {
             <Header />
             <div className="w-full flex justify-center">
 
+                <div className="flex flex-col bg-white md:w-6/12 shadow-sm shadow-[#242222] rounded-md w-full">
+                    <div className="w-fit self-center">
+                        <span className="text-3xl font-semibold">Barbeiros - Cadastro</span>
+                    </div>
+                    <div className="w-11/12 h-0.5 bg-black self-center mt-4 mb-4 opacity-5"></div>
+
+                    <form className="flex flex-col md:flex-row gap-4 p-2 md:w-11/12 w-full">
+                        <InputText
+                            label="Nome"
+                            type="text"
+                            className="md:w-64"
+                        />
+                    </form>
+                </div>
             </div>
         </div>
     )
